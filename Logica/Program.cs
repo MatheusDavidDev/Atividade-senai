08480ce3717e97573d942fe8a3c6d456fcd3aae4
matheus <matheusviniciusdavid@gmail.com>
2020-11-12 09:33:14 -0300

    Calcular a media do aluno

---
+++ Logica/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui vai o codigo
+
+            //Console.WriteLine("Digite o seu nome:");
+            //string nomeCompleto = Console.ReadLine();
+
+           //Console.WriteLine("Bem vindo "+nomeCompleto);
+        int valorUm;
+        int valorDois;
+
+                //entrada de dados
+        Console.WriteLine("Soma de dois numeros");
+        Console.WriteLine("--------------------");
+        Console.Write("Digite o primeiro: ");
+        valorUm = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        valorDois = int.Parse(Console.ReadLine());
+
+                //processamento
+        int soma = valorUm + valorDois;
+
+                //exibir
+        Console.WriteLine("A soma de "+valorUm+" + "+valorDois+" = "+soma);
+
+        }
+    }
+}
